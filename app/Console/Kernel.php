abe11d02733bfbf690aee81832e3867cb50bc3d3
路西法 <zhaiyuxin103@hotmail.com>
2021-07-13 10:43:52 +0800

    ✨ feat(user): 活跃用户

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        // 一小时执行一次『活跃用户』数据生成的命令
+        $schedule->command('larabbs:calculate-active-user')->hourly();
     }
 
     /**
@@ -34,7 +37,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
     use HasDateTimeFormatter;
 
+    use Traits\ActiveUserHelper;
+
     /**
      * The attributes that are mass assignable.
      *
